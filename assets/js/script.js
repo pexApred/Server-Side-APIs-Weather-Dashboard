29b985488bd8c9a3df9a0c7556761ca99a9d2193
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-04 22:45:38 -0500

    added new search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,21 @@ var cities = [];
 
 var initListeners = function(){
     console.log("init listeners");
+
+    $("new-search").submit(function(event){
+        event.preventDefault();
+        console.log("submitted form");
+
+        var newCity = $("#new-city").val();
+
+        var newSearch = {
+            city: newCity
+
+        };
+
+        addCity(newSearch)
     
+    });
 }
 
 $(function(){
