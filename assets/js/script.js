41f2fe781b1a5cac00698a3bdbe0e64eefeb85fc
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-06 13:13:59 -0500

    finished populateCityToday function to get current conditions for a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,13 @@ var cities = [];
 var addCity = function() {
     console.log("add city");
     var APIKey = "517b598cb370b4d60b6492926681f7ac";
-    var city = "Raleigh";
-    // var cityUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
-    var cityUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&limit=5&appid="+APIKey;
     
+    var city = $("#new-city").val();
+    console.log(city);
+
+    var cityUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&limit=5&appid="+APIKey;
+    // var cityDaysUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
+
     fetch(cityUrl)
         .then( function(response) {
             return response.json();
@@ -16,16 +19,58 @@ var addCity = function() {
         .then( function(data) {
             console.log("fetched city", data);
 
-            updateForm(data.city);
+            populateCityToday(data);
+            // updateForm(data.city);
         });
 
     
 };
 
-var updateForm = function(city){
-    console.log("update form:"+city+" ... ")
-    $("#new-city").val(city);
-};
+var populateCityToday = function(APIResponse){
+    console.log(APIResponse.city.name);
+    
+    var resultsToday = {
+        cityName: APIResponse.city.name,
+        currentDate: APIResponse.list[0].dt_txt.split(" ")[0],
+        cityTemp: APIResponse.list[0].main.temp,
+        cityWind: APIResponse.list[0].wind.speed,
+        cityHumidity: APIResponse.list[0].main.humidity,
+    };
+
+   var cityNameEL = document.createElement("h2");
+   cityNameEL.innerHTML = resultsToday.cityName +" "+ resultsToday.currentDate.split("-").reverse().join("/");
+   document.getElementById("weather-today").appendChild(cityNameEL);
+
+   var cityTempEL = document.createElement("p");
+   cityTempEL.innerHTML = "Temp: " + resultsToday.cityTemp;
+   document.getElementById("weather-today").appendChild(cityTempEL);
+
+   var cityWindEL = document.createElement("p");
+   cityWindEL.innerHTML = "Wind: " + resultsToday.cityWind;
+   document.getElementById("weather-today").appendChild(cityWindEL);
+
+   var cityHumidityEL = document.createElement("p");
+   cityHumidityEL.innerHTML = "Humidity: " + resultsToday.cityHumidity;
+   document.getElementById("weather-today").appendChild(cityHumidityEL);
+
+
+}
+
+// Create button to append 
+// var searchHistory = function(city){
+//     console.log("update form:"+city+" ... ")
+//     $("#new-city").val();
+// };
+
+var updateCityList = function(){
+    console.log("update city list");
+
+    var citiesHtml = "";
+    for (var i=0;i<cities.length; i++) {
+        citiesHtml += "<article class='cities' data-index='"+i+"'>";
+        citiesHtml += "<p class='city'>"+cities[i].city+'</p>';
+    };
+}
 
 var initListeners = function(){
     console.log("init listeners");
@@ -33,19 +78,10 @@ var initListeners = function(){
     $("#new-search").submit(function(event){
         event.preventDefault();
         console.log("submitted form");
-
-        var newCity = $("#new-city").val();
-
-        var newSearch = {
-            city: newCity
-
-        };
-
-        addCity(newSearch);
     
     });
 
-    $("#search-button").click()
+    $("#search-button").click(addCity);
 }
 
 $(function(){
